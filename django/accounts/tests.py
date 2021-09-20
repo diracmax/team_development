2bd6f8b0205495c8f1abaa2c624c9e683c1c1f7a
sasakiyudai <yudai14142@gmail.com>
2021-09-20 22:12:51 +0900

    add test

--- django/accounts/tests.py
+++ django/accounts/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import Client, TestCase
+from .models import CustomUser
+
+class HogeTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user('HogeTaro', 'taro@hoge.com', 'password')
+
+        self.client = Client()
+
+        self.client.login(username='HogeTaro', password='password')
+		
