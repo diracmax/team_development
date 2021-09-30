63e6db9fad01364f00881723fb4d04028d4a8231
tsukasakop <tsukasakop@gmail.com>
2021-09-30 13:32:22 +0900

    add list like,recruit,entry,join & link on profiel

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('edit/', views.edit, name='edit'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/<str:query>', views.PostList.as_view(), name='posts'),
 ]
