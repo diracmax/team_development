fed71d12c0a5b5d9256059f4ba6933c634d6e4fd
tsukasakop <tsukasakop@gmail.com>
2021-10-07 16:31:40 +0900

    ADD search page

---
+++ django/search/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from timeline.models import Post, Apply
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.db.models import Q
+
+
+
+class PostList(LoginRequiredMixin, generic.ListView):
+    template_name = 'search/post.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        q_word = self.request.GET.get('word')
+        if q_word:
+            object_list = Post.objects.filter(
+                Q(title__icontains=q_word) | Q(text__icontains=q_word))
+        else:
+            object_list = []
+        return object_list
