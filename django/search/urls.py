d467309734453d6d84dcd28aeaf65672734be5ae
tsukasakop <tsukasakop@gmail.com>
2021-10-08 09:43:20 +0900

    add user search

--- django/search/urls.py
+++ django/search/urls.py
@@ -6,5 +6,5 @@ app_name = 'search'
 urlpatterns = [
     path('post/', views.PostList.as_view(), name='post'),
     # path('post/advanced', views.PostList.as_view(), name='advanced'),
-    # path('accounts/', views.AccountList(), name='accounts'),
+    path('account/', views.AccountList.as_view(), name='account'),
 ]
--- django/search/views.py
+++ django/search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.messages.views import SuccessMessageMixin
 from timeline.models import Post, Apply
+from accounts.models import CustomUser
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.db.models import Q
@@ -16,6 +17,20 @@ class PostList(LoginRequiredMixin, generic.ListView):
         if q_word:
             object_list = Post.objects.filter(
                 Q(title__icontains=q_word) | Q(text__icontains=q_word))
+        else:
+            object_list = []
+        return object_list
+
+
+class AccountList(LoginRequiredMixin, generic.ListView):
+    template_name = 'search/account.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        q_word = self.request.GET.get('word')
+        if q_word:
+            object_list = CustomUser.objects.filter(
+                Q(username__icontains=q_word) | Q(description__icontains=q_word))
         else:
             object_list = []
         return object_list
