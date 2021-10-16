3022529706f103e9631119988fa1eda101723ded
tsukasakop <tsukasakop@gmail.com>
2021-10-16 20:39:23 +0900

    retouch category

--- django/timeline/views.py
+++ django/timeline/views.py
@@ -23,7 +23,7 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 class CreateView(LoginRequiredMixin, generic.CreateView):
     form_class = PostForm
     template_name = 'timeline/create_post.html'
-    # template_name = 'create_post2.html'
+    template_name = 'create_post2.html'
     success_url = reverse_lazy('timeline:index')
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
