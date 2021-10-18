95cfd5e55fb3f3786eb414b4ded565c1ed8163a6
tsukasakop <tsukasakop@gmail.com>
2021-10-18 10:07:49 +0900

    retouch search to fit categoryserch

--- django/search/views.py
+++ django/search/views.py
@@ -12,10 +12,23 @@ class PostList(LoginRequiredMixin, generic.ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        q_word = self.request.GET.get('word')
-        if q_word:
+        q = dict()
+        q["word"] = self.request.GET.get('word')
+        q["category"] = self.request.GET.get('category')
+        if q["word"] and q["category"]:
             object_list = Post.objects.filter(
-                Q(title__icontains=q_word) | Q(text__icontains=q_word))
-        else:
-            object_list = []
-        return object_list
+                    Q(title__icontains=q["word"]) | Q(text__icontains=q["word"]),
+                    Q(category__dispaly=q["category"])
+                    )
+            return object_list
+        if q["word"]:
+            object_list = Post.objects.filter(
+                    Q(title__icontains=q["word"]) | Q(text__icontains=q["word"])
+                    )
+            return object_list
+        if q["category"]:
+            object_list = Post.objects.filter(
+                    Q(category__display=q["category"])
+                    )
+            return object_list
+        return Post.objects.all()
