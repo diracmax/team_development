e13e8017d085597d4277e212d6cd646abc499cb3
tsukasakop <tsukasakop@gmail.com>
2021-10-22 12:56:56 +0900

    retouch migrate to setup db

--- django/timeline/apps.py
+++ django/timeline/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class TimelineConfig(AppConfig):
     name = 'timeline'
+
+    def ready(self):
+        from .models import create_default_category
+        post_migrate.connect(create_default_category, sender=self)
